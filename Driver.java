378bd3f1fa13c353bccc02fe4f62f14c8f474517
Kevin Li <kevinli4321@gmail.com>
2019-03-12 22:04:30 -0400

    Median optimization

--- Driver.java
+++ Driver.java
@@ -4,6 +4,6 @@ public class Driver {
     int[] arr2 = {22, 2, 3, 4, 6, 7, 10, 91, 3021, -9, 98421, -195};
     int[] arr3 = {999, 999, 999, 4, 1, 0, 3, 2, 999, 999, 999};
 
-    Quick.partition(arr, 0, 7);
+    System.out.println(Quick.partition(arr, 0, 7));
   }
 }
--- Quick.java
+++ Quick.java
@@ -6,36 +6,39 @@ public class Quick {
     //In range between start and end, inclusive
     Random randgen = new Random();
     int pivotIdx = Math.abs(randgen.nextInt() % (end - start + 1)) + start;
+    int[] nums = {start, pivotIdx, end};
+    Arrays.sort(nums);
+    pivotIdx = nums[1]; //the median
     System.out.println(pivotIdx);
     int pivot = data[pivotIdx];
-    swap(data, start, pivotIdx);
-    int i = start;
+    swap(data, start, pivotIdx); //swap to 0 index
+    int i = start; //since start and end change
     int j = end;
-    ++start;
+    ++start; //start is now 1 since 0 is the pivot
 
     while (start < end) {
-      if (data[start] == pivot)
+      if (data[start] == pivot) //prevent duplicates breaking the algorithm
         ++start;
 
       while (data[start] < pivot) {
+        //For the case where you get the maximum number which will cause an index exception for the while above
         if (start == end) {
-          swap(data, i, j);
+          swap(data, i, j); //just swap the pivot right to the end
           return end;
         }
 
         ++start;
       }
-      
+
       while (data[end] > pivot) --end;
       swap(data, start, end);
     }
 
-    swap(data, start, end);
-    swap(data, i, end);
+    swap(data, start, end); //for some reason I need to do this, otherwise it doesn't work
+    swap(data, i, end); //swap the pivot at idx 0 to the correct index
 
     System.out.println(Arrays.toString(data));
-    System.out.println(end);
-    return 1;
+    return end;
   }
 
   private static int[] swap(int[] data, int idx, int idx2) {

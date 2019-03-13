bb1c7dbd8d5a450b8a6120a563c3a8460978c126
Kevin Li <kevinli4321@gmail.com>
2019-03-12 20:03:04 -0400

    partition somewhat works

--- Quick.java
+++ Quick.java
@@ -2,24 +2,6 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class Quick {
-  public static int partitionLol(int[] data, int start, int end) {
-    //In range between start and end, inclusive
-    Random randgen = new Random();
-    int pivotIdx = Math.abs(randgen.nextInt() % (end - start + 1)) + start;
-
-    int pivot = data[pivotIdx];
-
-    int i = start; //don't really want to modify the actual start and end
-    int j = end;
-    while (i < j) {
-      while (data[i] < pivot) ++i; //keep going up until something should be right of the pivot
-      while (data[j] > pivot) --j; //keep going down until something should be left of the pivot
-      swap(data, i, j); //swap the two indexes from earlier to get them onto the correct side
-    }
-
-    return j; //new idx of the pivot
-  }
-
   public static int partition(int[] data, int start, int end) {
     //In range between start and end, inclusive
     Random randgen = new Random();
@@ -27,18 +9,18 @@ public class Quick {
     System.out.println(pivotIdx);
     int pivot = data[pivotIdx];
     swap(data, start, pivotIdx);
-    ++start;
+    int i = start + 1;
 
     while (start < end) {
-      if (data[start] < pivot) {
-        System.out.println(Arrays.toString(data));
-        swap(data, start, end);
-        ++start;
-        --end;
-      }
+      while (data[start] < pivot) ++start;
+      while (data[end] > pivot) --end;
+      swap(data, start, end);
     }
 
-    System.ou
+    swap(data, start, end);
+
+    System.out.println(Arrays.toString(data));
+    System.out.println(end);
     return 1;
   }
 
